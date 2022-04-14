24860ee2d0646f47a9b24bcbe9934175af2e648e
Redundancia <hunyadiwalter94@gmail.com>
2022-04-15 01:42:29 +0200

    Add day incremention for calculate_due_date

--- calculate_due_date.py
+++ calculate_due_date.py
@@ -1,4 +1,3 @@
-from calendar import THURSDAY
 from datetime import datetime, timedelta
 
 def calculate_due_date(submit_date, turnaround_time):
@@ -16,7 +15,8 @@ def calculate_due_date(submit_date, turnaround_time):
     WORKING_HOURS_END = 17
     WORKDAYS = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday")
     WORKING_HOURS_PER_DAY = 8
-    
+    DATETIME_FRIDAY_INTEGER = 4
+
     # validate submite_date type
     if not isinstance(submit_date, datetime):
         raise ValueError("Submit date is not a valid input, try a datetime.datetime type.")
@@ -25,7 +25,17 @@ def calculate_due_date(submit_date, turnaround_time):
     if not isinstance(turnaround_time, int) or turnaround_time <= 0:
         raise ValueError("Turnaround time is not a valid input, try a positive integer in hours.")
 
-    return datetime.now()
+    # return date variable
+    dueDate = submit_date
+
+    # while at least a whole working day left, add 1 day to date, and skip weekends if needed
+    while turnaround_time >= WORKING_HOURS_PER_DAY:
+        dueDate = dueDate + timedelta(days=+1)
+        if dueDate.weekday() > DATETIME_FRIDAY_INTEGER:
+            dueDate = dueDate + timedelta(days=+2)
+        turnaround_time -= WORKING_HOURS_PER_DAY
+
+    return dueDate
 
 if __name__ == "__main__":
     calculate_due_date(datetime.now(), 1)
--- test_calculate_due_date.py
+++ test_calculate_due_date.py
@@ -1,5 +1,5 @@
 from calculate_due_date import calculate_due_date
-from datetime import datetime, timedelta
+from datetime import datetime
 import pytest
 
 def test_invalid_submit_date():
@@ -22,5 +22,21 @@ def test_invalid_submit_date_not_int():
     with pytest.raises(ValueError, match="Turnaround time is not a valid input, try a positive integer in hours."):
         calculate_due_date(datetime.now(), 1.11)
 
-def valid_submit_date_and_turnaround_time_valid_return_type():
+def test_valid_submit_date_and_turnaround_time_valid_return_type():
     assert isinstance(calculate_due_date(datetime.now(), 12), datetime)
+
+# From friday 10:30 to same day 10:30
+def test_valid_0_day():
+    assert calculate_due_date(datetime(2022,4,15,10,30), 2) == datetime(2022,4,15,10,30)
+
+# From friday 10:30 to monday 10:30
+def test_valid_friday_to_monday():
+    assert calculate_due_date(datetime(2022,4,15,10,30), 8) == datetime(2022,4,18,10,30)
+
+# From last friday of month to next months
+def test_go_to_next_month():
+    assert calculate_due_date(datetime(2022,4,29,10,30), 8) == datetime(2022,5,2,10,30)
+
+# From tuesday to friday
+def test_valid_3_day_same_week():
+    assert calculate_due_date(datetime(2022,4,12,10,30), 24) == datetime(2022,4,15,10,30)
