6fd199a75c8dc559d77c019499001dcd7aed180b
Redundancia <hunyadiwalter94@gmail.com>
2022-04-15 03:53:51 +0200

    Refactor

--- calculate_due_date.py
+++ calculate_due_date.py
@@ -17,6 +17,34 @@ def calculate_due_date(submit_date, turnaround_time):
     WORKING_HOURS_PER_DAY = 8
     DATETIME_FRIDAY_INTEGER = 4
 
+    validate_inputs(submit_date, turnaround_time, WORKING_HOURS_START, WORKING_HOURS_END, DATETIME_FRIDAY_INTEGER)
+
+    # setup return date variable
+    due_date = submit_date
+
+    # while at least a whole working day left, add 1 day to date, and skip weekends if needed
+    while turnaround_time >= WORKING_HOURS_PER_DAY:
+        due_date = due_date + timedelta(days=+1)
+        due_date = skip_weekend_if_needed(due_date, DATETIME_FRIDAY_INTEGER)
+        turnaround_time -= WORKING_HOURS_PER_DAY
+
+    # add the rest of the hours to the date
+    due_date += timedelta(hours=turnaround_time)
+    
+    # if time after workhours, add 1 extra day and extract time difference from hour
+    if due_date.time() > WORKING_HOURS_END:
+        due_date += timedelta(days=1, hours=(WORKING_HOURS_START.hour - WORKING_HOURS_END.hour))
+
+    # If we go into next day, but we are before starting work hours, add a day and extract daily working hours
+    if due_date.time() == AM_12:
+        due_date += timedelta(days=1, hours=-WORKING_HOURS_PER_DAY)
+
+    due_date = skip_weekend_if_needed(due_date, DATETIME_FRIDAY_INTEGER)
+
+    return due_date
+
+
+def validate_inputs(submit_date, turnaround_time, WORKING_HOURS_START, WORKING_HOURS_END, DATETIME_FRIDAY_INTEGER):
     # validate submite_date type
     if not isinstance(submit_date, datetime):
         raise ValueError("Submit date is not a valid input, try a datetime.datetime type.")
@@ -28,36 +56,17 @@ def calculate_due_date(submit_date, turnaround_time):
     # validate submit date value for weekdays
     if submit_date.weekday() > DATETIME_FRIDAY_INTEGER:
         raise ValueError("Invalid submit date, try weekdays:Mon-Fri.")
+
     #validate submit date value for working hours
     if submit_date.time() < WORKING_HOURS_START or submit_date.time() > WORKING_HOURS_END:
         raise ValueError("Invalid submit date time, try working hours:9-17.")
 
-    # setup return date variable
-    dueDate = submit_date
 
-    # while at least a whole working day left, add 1 day to date, and skip weekends if needed
-    while turnaround_time >= WORKING_HOURS_PER_DAY:
-        dueDate = dueDate + timedelta(days=+1)
-        if dueDate.weekday() > DATETIME_FRIDAY_INTEGER:
-            dueDate = dueDate + timedelta(days=+2)
-        turnaround_time -= WORKING_HOURS_PER_DAY
-
-    # add the rest of the hours to the date
-    dueDate += timedelta(hours=turnaround_time)
-    
-    # if time after workhours, add 1 extra day and extract time difference from hour
-    if dueDate.time() > WORKING_HOURS_END:
-        dueDate += timedelta(days=1, hours=(WORKING_HOURS_START.hour - WORKING_HOURS_END.hour))
-
-    # If we go into next day, but we are before starting work hours, add a day and extract daily working hours
-    if dueDate.time() == AM_12:
-        dueDate += timedelta(days=1, hours=-WORKING_HOURS_PER_DAY)
-
-    # skip weekend if needed
+def skip_weekend_if_needed(dueDate, DATETIME_FRIDAY_INTEGER):
     if dueDate.weekday() > DATETIME_FRIDAY_INTEGER:
         dueDate = dueDate + timedelta(days=+2)
-
     return dueDate
+
 
 if __name__ == "__main__":
     calculate_due_date(datetime(2022,4,13,15,30),11)
